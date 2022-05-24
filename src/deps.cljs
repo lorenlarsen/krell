7500aeeccca4f744834c508d33ea23eed5a2c5b9
lorenlarsen <loren@lorenlarsen.com>
2022-05-23 23:25:20 -0600

    Update deps.cljs

--- src/deps.cljs
+++ src/deps.cljs
@@ -1,2 +1,2 @@
-{:npm-deps {"react-native-tcp-socket" "5.6.0"
-            "react-native-device-info" "8.1.2"}}
+{:npm-deps {"react-native-tcp-socket" "5.6.2"
+            "react-native-device-info" "8.7.1"}}
